51c571732ce2d678ad2b08e77a0cb3170511f2db
sam <samwhite@uw.edu>
2019-01-19 21:13:30 -0800

    add trinity subsetting

---
+++ scripts/20190119_porites_blast_faidx_trinity.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+# Pull out coral transcripts based on BLAST results
+
+## Set input file locations
+transcriptome=/media/sam/4TB_toshiba/porites/20180419_trinity/Trinity.fasta
+transcriptome_index=/media/sam/4TB_toshiba/porites/20180419_trinity/Trinity.fasta.fai
+transcripts_list=/media/sam/4TB_toshiba/porites/20190115_blastx_ncbi_nr-v5_porites/20190115_blastx_nr_porites.list
+
+## Set output file locations/names
+out_dir=/media/sam/4TB_toshiba/porites
+out_fasta=${out_dir}/Trinity.coral.fasta
+out_fai=${out_dir}/Trinity.coral.fasta.fai
+
+## Set program locations
+faidx="/home/shared/samtools-1.9/samtools faidx"
+
+cd ${out_dir}
+
+# Pull out coral only seqs from Trinity transcriptome
+while read contig; do \
+${faidx} \
+${transcriptome_index} \
+${contig} \
+>> ${out_fasta}; \
+done < ${transctipts_list}
