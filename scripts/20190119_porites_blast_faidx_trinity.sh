f6ac2d66348cfd1b061166965919416be97b7ce2
kubu4 <samuel.j.white@gmail.com>
2019-01-23 06:23:14 -0800

    Rename variables; remove extraneous variable.

--- scripts/20190119_porites_blast_faidx_trinity.sh
+++ scripts/20190119_porites_blast_faidx_trinity.sh
@@ -7,9 +7,8 @@ transcriptome=/media/sam/4TB_toshiba/porites/20180419_trinity/Trinity.fasta
 transcripts_list=/media/sam/4TB_toshiba/porites/20190115_blastx_ncbi_nr-v5_porites/20190115_blastx_nr_porites.list
 
 ## Set output file locations/names
-out_dir=/media/sam/4TB_toshiba/porites
-out_fasta=${out_dir}/Trinity.coral.fasta
-out_fai=${out_dir}/Trinity.coral.fasta.fai
+out_dir=/media/sam/4TB_toshiba/porites/coral_transcripts/
+coral_transcriptome=${out_dir}/Trinity.coral.fasta
 
 ## Set program locations
 faidx="/home/shared/samtools-1.9/samtools faidx"
@@ -21,8 +20,11 @@ while read contig; do
   ${faidx} \
   ${transcriptome} \
   ${contig} \
-  >> ${out_fasta}
+  >> ${coral_transcriptome}
 done < ${transcripts_list}
 
 ## Create index for new fasta
-${faidx} ${out_fasta}
+${faidx} ${coral_transcriptome}
+
+# Email me when job is complete
+sed '/^Subject:/ s/ / porites_faidx JOB COMPLETE/' ~/.default-subject.mail | msmtp "$EMAIL"
