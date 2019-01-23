5c452b038f512176678f1517d057b6b2ee2bf84e
sam <samwhite@uw.edu>
2019-01-23 14:04:11 -0800

    rm extraneous variable; update output path

--- scripts/20190119_montipora_blast_faidx_trinity.sh
+++ scripts/20190119_montipora_blast_faidx_trinity.sh
@@ -6,9 +6,8 @@
 transcriptome=/media/sam/4TB_toshiba/montipora/20180416_trinity/Trinity.fasta
 transcripts_list=/media/sam/4TB_toshiba/montipora/20190105_blastx_ncbi_nr-v5_montipora/20190105_blastx_nr_montipora.list
 ## Set output file locations/names
-out_dir=/media/sam/4TB_toshiba/montipora
+out_dir=/media/sam/4TB_toshiba/montipora/coral_transcripts
 out_fasta=${out_dir}/Trinity.coral.fasta
-out_fai=${out_dir}/Trinity.coral.fasta.fai
 
 ## Set program locations
 faidx="/home/shared/samtools-1.9/samtools faidx"
