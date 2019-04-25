c0db839afa5756ab70cde373922f4fd9f2e4535e
sam <samwhite@uw.edu>
2019-04-25 09:51:10 -0700

    set up variables

--- scripts/20190425_montipora_symbio_inparanoid.sh
+++ scripts/20190425_montipora_symbio_inparanoid.sh
@@ -2,11 +2,15 @@
 
 
 
-inparanoid_gzip_loc="/home/shared/inparanoid_4.1.tar.gz"
+inparanoid_coral_rejects="/media/sam/4TB_toshiba/montipora/20181204_inparanoid/inparanoid_4.1/rejected_sequences.symbB.v1.2.augustus.prot.fa"
+trinity_fasta="/media/sam/4TB_toshiba/montipora/20180416_trinity"
+trinity_fai="/media/sam/4TB_toshiba/montipora/20180416_trinity.fai"
 
+symbio_trinity_list="symbio-list.txt"
+symbio_fasta="symbio-monitpora"
 
-cp ${inparanoid_gzip_loc} .
-
-gunzip inparanoid_4.1.tar.gz
-
-cd inparanoid_4.1
+${inparanoid_coral_rejects} \
+| tr " " "." \
+| tr "(" "." \
+| awk -F"." '{ print $5 }' \
+> ${symbio_trinity_list}
