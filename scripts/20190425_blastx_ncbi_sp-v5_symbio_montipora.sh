847e5efb6c842873c91300ad931374269622b5ef
sam <samwhite@uw.edu>
2019-04-25 20:38:13 -0700

    set variables

--- scripts/20190425_blastx_ncbi_sp-v5_symbio_montipora.sh
+++ scripts/20190425_blastx_ncbi_sp-v5_symbio_montipora.sh
@@ -1,1 +1,13 @@
 #!/bin/env bash
+
+# Input/output files
+blastx_out_dir=/media/sam/4TB_toshiba/montipora/20190425_blastx_ncbi_sp-v5_symbio_montipora
+taxid_list=/media/sam/4TB_toshiba/montipora/20190425_blastx_ncbi_sp-v5_symbio_montipora/dinoflagellate.taxids
+orf_fasta=/media/sam/4TB_toshiba/montipora/20180429_transdecoder/Trinity.fasta.transdecoder.cds.complete-ORFS-only.fasta
+
+# Programs variables
+get_taxids=/home/shared/ncbi-blast-2.8.1+/bin/get_species_taxids.sh
+blastx=/home/shared/ncbi-blast-2.8.1+/bin/blastx
+blastdb_dir=/mnt/data/ncbi_swissprot_v5_db
+sp_db=swissprot_v5
+blastx_out_file=20190425_blastx_ncbi_sp-v5_symbio_montipora.tab
