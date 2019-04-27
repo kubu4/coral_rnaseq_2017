7ca3fd5109f638ecbf50386555d0c9f0cc65bd6e
sam <samwhite@uw.edu>
2019-04-26 20:42:51 -0700

    modify transcriptome variable name

--- scripts/20190426_symbio_monitpora_blast_faidx_trinity.sh
+++ scripts/20190426_symbio_monitpora_blast_faidx_trinity.sh
@@ -9,7 +9,7 @@ transcriptome=/media/sam/4TB_toshiba/montipora/20180416_trinity/Trinity.fasta
 ## Set output file locations/names
 out_dir=/media/sam/4TB_toshiba/montipora/20190426_symbio_montipora_transcripts
 transcripts_list=/media/sam/4TB_toshiba/montipora/20190426_symbio_montipora_transcripts/20190426_symbio_montipora_transcripts.list
-coral_transcriptome=${out_dir}/Trinity.sp.symbio_montipora.fasta
+sybmio_transcriptome=${out_dir}/Trinity.sp.symbio_montipora.fasta
 
 ## Set program locations
 faidx="/home/shared/samtools-1.9/samtools faidx"
@@ -26,7 +26,7 @@ while read contig; do
   ${faidx} \
   ${transcriptome} \
   ${contig} \
-  >> ${coral_transcriptome}
+  >> ${symbio_transcriptome}
 done < ${transcripts_list}
 
 ## Create index for new fasta
