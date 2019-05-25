6614f99e15e8fd854a7a4793a9a53b86e00a3835
sam <samwhite@uw.edu>
2019-05-25 14:17:10 -0700

    sed commands for config file changes

--- scripts/20190525_inparanoid_all_orfs.sh
+++ scripts/20190525_inparanoid_all_orfs.sh
@@ -5,5 +5,15 @@
 
 # Exit script if a command fails
 set -e
-# Save current directory as working directory.
-wd="$(pwd)"
+
+
+# Use sed to modify InParanoid config file
+# Uses the "%" as the substitute delimiter to allow usage of "/" in paths
+## Set blastall location
+sed -i '/^$blastall = "blastall"/ s%"blastall"%"/home/shared/blast-2.2.17/bin/blastall -a23"%' inparanoid.pl
+
+## Set formatdb location
+sed -i '/^$formatdb = "formatdb"/ s%"formatdb"%"/home/shared/blast-2.2.17/bin/formatdb"%' inparanoid.pl
+
+## Set InParanoid to use an out group (change from 0 to 1)
+sed -i '/^$use_outgroup = 0/ s%0%1%' inparanoid.pl
