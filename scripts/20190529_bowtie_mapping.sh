49140d3670700095041767b0479b550d0973cac3
Sam White <samuel.j.white@gmail.com>
2019-05-29 10:40:59 -0700

    update program paths, fastq name pattern

--- scripts/20190529_bowtie_mapping.sh
+++ scripts/20190529_bowtie_mapping.sh
@@ -6,17 +6,11 @@ set -e
 threads=28
 
 # Paths to programs
-blast_dir="/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin"
-blastx="${blast_dir}/blastx"
-seqtk="/gscratch/srlab/programs/seqtk-1.3/seqtk"
-
-# Paths to blastdbs
-blastdb_dir="/gscratch/srlab/blastdbs/ncbi-sp-v5"
-blast_db="${blastdb_dir}/swissprot_v5"
-samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
+bowtie2="/home/shared/bowtie2-2.3.4.1-linux-x86_64/bowtie2"
+samtools="/home/shared/samtools-1.9/samtools"
 
 # Input files
-fastq_dir="/gscratch/srlab/sam/data/metagenomics/P_generosa/"
+fastq_dir="/media/sam/4TB_toshiba/porites/"
 
 
 ## Inititalize arrays
@@ -25,13 +19,13 @@ fastq_array_R2=()
 names_array=()
 
 # Create array of fastq R1 files
-for fastq in ${fastq_dir}/*R1*.gz
+for fastq in ${fastq_dir}/*READ1*.gz
 do
   fastq_array_R1+=(${fastq})
 done
 
 # Create array of fastq R2 files
-for fastq in ${fastq_dir}/*R2*.gz
+for fastq in ${fastq_dir}/*READ2*.gz
 do
   fastq_array_R2+=(${fastq})
 done
@@ -39,7 +33,7 @@ done
 # Create array of sample names
 ## Uses parameter substitution to strip leading path from filename
 ## Uses awk to parse out sample name from filename
-for R1_fastq in ${fastq_dir}/*R1*.gz
+for R1_fastq in ${fastq_dir}/*READ1*.gz
 do
   names_array+=($(echo ${R1_fastq#${fastq_dir}} | awk -F"_" '{print $3 $4}'))
 done
