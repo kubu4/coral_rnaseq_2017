7b9559015a6ee58a2903a3a2e87461bc0db8dfb2
Sam White <samuel.j.white@gmail.com>
2019-06-04 15:13:57 -0700

    switch to isoform expre matrix, since that seems to fix gene lengths

--- scripts/20190603_porites_all_salmon_DEG_b_vs_nb_GO.sh
+++ scripts/20190603_porites_all_salmon_DEG_b_vs_nb_GO.sh
@@ -15,7 +15,7 @@ fasta_seq_lengths="${transcriptome_dir}/Trinity.fasta.seq_lens"
 samples="/home/sam/gitrepos/coral_rnaseq_2017/scripts/porites_b_vs_nb_trinity_sample_list.txt"
 
 gene_map="${transcriptome_dir}/Trinity.fasta.gene_trans_map"
-salmon_matrix="${salmon_out_dir}/salmon.gene.TMM.EXPR.matrix"
+salmon_matrix="${salmon_out_dir}/salmon.isoform.TMM.EXPR.matrix"
 go_annotations="/media/sam/4TB_toshiba/porites/20190530_trinotate_porites_all/go_annotations.txt"
 
 
