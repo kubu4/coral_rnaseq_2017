08dde6fee0f0bf29c107a34475ede264505711ad
Sam White <samuel.j.white@gmail.com>
2019-06-05 08:19:53 -0700

    add stderr/stdout for all commands

--- scripts/20190603_porites_all_salmon_DEG_b_vs_nb_GO.sh
+++ scripts/20190603_porites_all_salmon_DEG_b_vs_nb_GO.sh
@@ -19,8 +19,17 @@ salmon_matrix="${salmon_out_dir}/salmon.isoform.TMM.EXPR.matrix"
 go_annotations="/media/sam/4TB_toshiba/porites/20190530_trinotate_porites_all/go_annotations.txt"
 
 
-salmon_stdout="stdout.txt"
-salmon_stderr="stderr.txt"
+# Standard output/error files
+diff_expr_stdout="diff_expr_stdout.txt"
+diff_expr_stderr="diff_expr_stderr.txt"
+matrix_stdout="matrix_stdout.txt"
+matrix_stderr="matrix_stderr.txt"
+salmon_stdout="salmon_stdout.txt"
+salmon_stderr="salmon_stderr.txt"
+tpm_length_stdout="tpm_length_stdout.txt"
+tpm_length_stderr="tpm_length_stderr.txt"
+trinity_DE_stdout="trinity_DE_stdout.txt"
+trinity_DE_stderr="trinity_DE_stderr.txt"
 
 edgeR_dir=""
 
@@ -74,21 +83,27 @@ non-bleached_44_02/quant.sf \
 non-bleached_44_03/quant.sf \
 non-bleached_K5_01/quant.sf \
 non-bleached_K5_02/quant.sf \
-non-bleached_K5_03/quant.sf
+non-bleached_K5_03/quant.sf \
+1> ${matrix_stdout} \
+2> ${matrix_stderr}
 
 # Generate weighted gene lengths
 "${trinity_tpm_length}" \
 --gene_trans_map "${gene_map}" \
 --trans_lengths "${fasta_seq_lengths}" \
 --TPM_matrix "${salmon_matrix}" \
-> Trinity.gene_lengths.txt
+> Trinity.gene_lengths.txt \
+1> ${tpm_length_stdout} \
+2> ${tpm_length_stderr}
 
 # Differential expression analysis
 cd ${transcriptome_dir}
 ${trinity_DE} \
 --matrix ${salmon_out_dir}/salmon.isoform.counts.matrix \
 --method edgeR \
---samples_file ${samples}
+--samples_file ${samples} \
+1> ${trinity_DE_stdout} \
+2> ${trinity_DE_stderr}
 
 mv edgeR* ${salmon_out_dir}
 
@@ -100,7 +115,7 @@ mv edgeR* ${salmon_out_dir}
 # Pulls edgeR directory name and removes leading ./ in find output
 cd ${salmon_out_dir}
 edgeR_dir=$(find . -type d -name "edgeR*" | sed 's%./%%')
-cd ${edgeR_dir}
+cd "${edgeR_dir}"
 ${diff_expr} \
 --matrix "${salmon_matrix}" \
 --samples ${samples} \
@@ -108,7 +123,9 @@ ${diff_expr} \
 --GO_annots "${go_annotations}" \
 --gene_lengths ${salmon_out_dir}/Trinity.gene_lengths.txt \
 -C 1 \
--P 0.05
+-P 0.05 \
+1> ${diff_expr_stdout} \
+2> ${diff_expr_stderr}
 
 # Email me when job is complete
 sed '/^Subject:/ s/ / JOB COMPLETE/' ~/.default-subject.mail | msmtp "$EMAIL"
